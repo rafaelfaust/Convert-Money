2894eb5d9a11fa0a620c24341105906920c1b29c
Webfael <rafaelfaustrodrigues@gmail.com>
2022-12-22 14:32:09 -0300

    Finish

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+const dolar = 5.2
+const euro = 5.9
+const Bticoin = 86050.99
+
+convertValues = () => {
+    const input = document.getElementById('input-value').value
+    const realValueText = document.getElementById('real-value-text')
+    const converterValueText = document.getElementById('convert-value-text')
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(input);
+
+    if (select.value === 'US$ Dolar Americano') {
+        converterValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(input / dolar);
+    }
+    else if (select.value === '€ Euro' ) {
+        converterValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(input / dolar);
+    }
+    else {
+        converterValueText.innerHTML = input / Bticoin
+    }
+}
+
+changeCurrency = () => {
+    const nameText = document.getElementById('name-text')
+    const convertImg = document.getElementById('convert-img')
+
+    if (select.value === '€ Euro') {
+        nameText.innerHTML = 'Euro'
+        convertImg.src = './images/Euro.png'
+    }
+    else if (select.value === 'Bitcoin') {
+        nameText.innerHTML = 'Bitcoin'
+        convertImg.src = './images/Bit.png'
+    }
+    else {
+        nameText.innerHTML = 'Dollar Americano'
+        convertImg.src = './images/Usa.png'
+    }
+    convertValues()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
