be4c00bf38331e3b753c0bcad43686fd7aed2f14
Webfael <106990982+Webfael@users.noreply.github.com>
2023-01-21 18:06:30 -0300

    Script

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
-const dolar = 5.19
-const euro = 5.49
-const Bticoin = 0.000012
 
-convertValues = () => {
+const data = fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json() )
+
+const convertValues = async () => {
     const input = document.getElementById('input-value').value
     const realValueText = document.getElementById('real-value-text')
     const converterValueText = document.getElementById('convert-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json() )
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const Bticoin = data.BTCBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
